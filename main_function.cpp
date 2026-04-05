7dd2575922cc227f151671123f94bb46995bee85
Pavan Gandi <pavansai738291@gmail.com>
2026-04-05 22:27:36 +0530

    Update main_function.cpp

--- main_function.cpp
+++ main_function.cpp
@@ -18,33 +18,45 @@ vector<string> buildPath(string src, string dest, map<string,string> &parent)
  
 int main()
 {
-    int n, m;
-    cout << "Enter number of nodes: ";
-    cin >> n;
-    cout << "Enter number of edges: ";
-    cin >> m;
- 
-    graph g(n, m);
-    cout << "Enter edges (node1 node2 weight):\n";
-    g.fill();
+    graph g(0, 0);
  
     int choice;
     do
     {
-        cout << "1. Add Edge\n";
-        cout << "2. Show Network\n";
-        cout << "3. Shortest Path (Dijkstra)\n";
-        cout << "4. Simulate Packet\n";
-        cout << "5. Simulate Packet with Delay\n";
-        cout << "6. MST Cost (Kruskal)\n";
-        cout << "7. Save Graph to File\n";
-        cout << "8. Load Graph from File\n";
-        cout << "9. Exit\n";
+        cout << "\n--- Menu ---\n";
+        cout << "1. Enter Graph (nodes and edges)\n";
+        cout << "2. Load Graph from File\n";
+        cout << "3. Add Edge\n";
+        cout << "4. Show Network\n";
+        cout << "5. Shortest Path (Dijkstra)\n";
+        cout << "6. Simulate Packet\n";
+        cout << "7. Simulate Packet with Delay\n";
+        cout << "8. MST Cost (Kruskal)\n";
+        cout << "9. Save Graph to File\n";
+        cout << "10. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
         cin.ignore();
  
         if(choice == 1)
+        {
+            int n, m;
+            cout << "Enter number of nodes: ";
+            cin >> n;
+            cout << "Enter number of edges: ";
+            cin >> m;
+            g = graph(n, m);
+            cout << "Enter edges (node1 node2 weight):\n";
+            g.fill();
+        }
+        else if(choice == 2)
+        {
+            string fname;
+            cout << "Enter filename: ";
+            getline(cin, fname);
+            g = loadGraphFromFile(fname);
+        }
+        else if(choice == 3)
         {
             string a, b;
             int w;
@@ -53,11 +65,11 @@ int main()
             g.addedge(a, b, w);
             cout << "Edge added!\n";
         }
-        else if(choice == 2)
+        else if(choice == 4)
         {
             g.print();
         }
-        else if(choice == 3)
+        else if(choice == 5)
         {
             string src, dest;
             cout << "Enter source: ";
@@ -83,7 +95,7 @@ int main()
                 logRouteToFile("routes.txt", src, dest, path, dist[dest]);
             }
         }
-        else if(choice == 4)
+        else if(choice == 6)
         {
             string src, dest;
             cout << "Enter source: ";
@@ -107,7 +119,7 @@ int main()
                 simulatepacket(p);
             }
         }
-        else if(choice == 5)
+        else if(choice == 7)
         {
             string src, dest;
             cout << "Enter source: ";
@@ -131,27 +143,20 @@ int main()
                 simulatepacketwithdelay(p, g);
             }
         }
-        else if(choice == 6)
+        else if(choice == 8)
         {
             int cost = kruskalMST(g);
             cout << "MST cost: " << cost << "\n";
             saveMSTToFile("mst.txt", cost);
         }
-        else if(choice == 7)
+        else if(choice == 9)
         {
             string fname;
             cout << "Enter filename: ";
             getline(cin, fname);
             saveGraphToFile(g, fname);
         }
-        else if(choice == 8)
-        {
-            string fname;
-            cout << "Enter filename: ";
-            getline(cin, fname);
-            g = loadGraphFromFile(fname);
-        }
-        else if(choice == 9)
+        else if(choice == 10)
         {
             cout << "Bye!\n";
         }
@@ -160,7 +165,7 @@ int main()
             cout << "Invalid choice\n";
         }
  
-    } while(choice != 9);
+    } while(choice != 10);
  
     return 0;
 }
