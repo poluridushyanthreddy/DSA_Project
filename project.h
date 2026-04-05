217d89e2835d9e19886c95ab8df29ae49fe44bec
Pavan Gandi <pavansai738291@gmail.com>
2026-04-05 22:10:13 +0530

    Update project.h

--- project.h
+++ project.h
@@ -1,25 +1,26 @@
 #ifndef PROJECT_H
 #define PROJECT_H
-
 #include <bits/stdc++.h>
 using namespace std;
-
 // -------- GRAPH --------
 class graph {
 public:
     int n, m;
-    vector<vector<pair<char,int>>> grh;
-
+    map<string, vector<pair<string,int>>> grh;
     graph(int n, int m);
-    void addedge(char a, char b, int c);
+    void addedge(string a, string b, int c);
     void fill();
     void print();
-    int getWeight(char u, char v);
+    int getWeight(string u, string v);
+    vector<pair<string,int>> getNeighbours(string node);
+    vector<string> getNodes();
+    vector<tuple<string,string,int>> getAllEdges();
 };
-
 // -------- DIJKSTRA --------
-map<char,int> dijkstra(graph &g, char src, map<char,char> &parent);
-
+map<string,int> dijkstra(graph &g, string src, map<string,string> &parent);
+void printPath(string dest, map<string,string> &parent);
+// -------- MST --------
+int kruskalMST(graph &g);
 // -------- PACKET --------
 class packet {
 public:
@@ -29,9 +30,12 @@ public:
     vector<char> path;
     int total_time;
     bool delivered;
-
     packet(char s, char d);
 };
+// -------- SIMULATION --------
+void simulatepacket(packet &p);
+void simulatepacketwithdelay(packet &p, graph &g);
+#endif
 
 // -------- SIMULATION --------
 void simulatepacket(packet &p);
