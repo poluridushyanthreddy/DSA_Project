3cd0e66da2d1af112243533ec5b4d16bb23e14fa
saubanalam24 <saubanalam121@gmail.com>
2026-04-05 20:00:57 +0530

    I’ve aligned my algorithms with your graph structure (graph class, getNeighbours, getAllEdges). Everything should integrate and run properly now.

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,48 +1,125 @@
 #include "project.h"
+#include <bits/stdc++.h>
+using namespace std;
 
-
-map<char,int> dijkstra(graph &g, char src, map<char,char> &parent)
+map<string,int> dijkstra(graph &g, string src, map<string,string> &parent)
 {
-    map<char,int> distance;
+    map<string,int> distance;
 
-    for(int i=0;i<g.n;i++)
+    for(auto node : g.getNodes())
     {
-        char node=char(i + 65);
-        distance[node]=INT_MAX;
+        distance[node] = INT_MAX;
     }
 
-    priority_queue<pair<int,char>,vector<pair<int,char>>,greater<pair<int,char>>>pq;
+    priority_queue<pair<int,string>, vector<pair<int,string>>, greater<pair<int,string>>> pq;
 
-    distance[src]=0;
-    parent[src]='\0';
+    distance[src] = 0;
+    parent[src] = "";
     pq.push({0, src});
 
     while(!pq.empty())
     {
-        pair<int,char> current=pq.top();
+        pair<int,string> current = pq.top();
         pq.pop();
 
-        int currDist=current.first;
-        char node=current.second;
+        int currDist = current.first;
+        string node = current.second;
 
         if(currDist > distance[node])
             continue;
 
-        for(auto edge : g.grh[node - 65])
+        for(auto edge : g.getNeighbours(node))
         {
-            char nextNode=edge.first;
-            int weight=edge.second;
+            string nextNode = edge.first;
+            int weight = edge.second;
 
-            int newDist=currDist + weight;
+            int newDist = currDist + weight;
 
             if(newDist < distance[nextNode])
             {
-                distance[nextNode]=newDist;
-                parent[nextNode]=node;
-                pq.push({newDist,nextNode});
+                distance[nextNode] = newDist;
+                parent[nextNode] = node;
+                pq.push({newDist, nextNode});
             }
         }
     }
 
     return distance;
+}
+
+void printPath(string dest, map<string,string> &parent)
+{
+    if(dest == "")
+        return;
+
+    printPath(parent[dest], parent);
+    cout << dest << " ";
+}
+
+int kruskalMST(graph &g)
+{
+    vector<tuple<int,string,string>> edges;
+
+    for(auto e : g.getAllEdges())
+    {
+        string u, v;
+        int w;
+        tie(u, v, w) = e;
+
+        edges.push_back({w, u, v});
+    }
+
+    sort(edges.begin(), edges.end());
+
+    map<string,string> parent;
+    map<string,int> rank;
+
+    for(auto node : g.getNodes())
+    {
+        parent[node] = node;
+        rank[node] = 0;
+    }
+
+    function<string(string)> findSet = [&](string x)
+    {
+        if(parent[x] != x)
+            parent[x] = findSet(parent[x]);
+        return parent[x];
+    };
+
+    function<void(string,string)> unionSet = [&](string a, string b)
+    {
+        string pa = findSet(a);
+        string pb = findSet(b);
+
+        if(pa == pb)
+            return;
+
+        if(rank[pa] < rank[pb])
+            parent[pa] = pb;
+        else if(rank[pa] > rank[pb])
+            parent[pb] = pa;
+        else
+        {
+            parent[pb] = pa;
+            rank[pa]++;
+        }
+    };
+
+    int totalCost = 0;
+
+    for(auto e : edges)
+    {
+        int w;
+        string u, v;
+        tie(w, u, v) = e;
+
+        if(findSet(u) != findSet(v))
+        {
+            unionSet(u, v);
+            totalCost += w;
+        }
+    }
+
+    return totalCost;
 }
