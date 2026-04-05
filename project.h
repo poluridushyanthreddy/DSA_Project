0fbb60fdc7a6cc76791e7aab7d028cf897783531
Yaswanth064 <bomburiyaswanth@gmail.com>
2026-04-05 22:17:29 +0530

    Refactor project.h to clean up function declarations
    Removed duplicate simulation function declarations and fixed header guards.

--- project.h
+++ project.h
@@ -1,8 +1,11 @@
 #ifndef PROJECT_H
 #define PROJECT_H
 #include <bits/stdc++.h>
+
 using namespace std;
+
 // -------- GRAPH --------
+
 class graph {
 public:
     int n, m;
@@ -16,12 +19,18 @@ public:
     vector<string> getNodes();
     vector<tuple<string,string,int>> getAllEdges();
 };
+
 // -------- DIJKSTRA --------
+
 map<string,int> dijkstra(graph &g, string src, map<string,string> &parent);
 void printPath(string dest, map<string,string> &parent);
+
 // -------- MST --------
+
 int kruskalMST(graph &g);
+
 // -------- PACKET --------
+
 class packet {
 public:
     char source;
@@ -32,12 +41,13 @@ public:
     bool delivered;
     packet(char s, char d);
 };
-// -------- SIMULATION --------
-void simulatepacket(packet &p);
-void simulatepacketwithdelay(packet &p, graph &g);
-#endif
 
 // -------- SIMULATION --------
+
+void simulatepacket(packet &p);
+void simulatepacketwithdelay(packet &p, graph &g);
+
+#endif
 void simulatepacket(packet &p);
 void simulatepacketwithdelay(packet &p, graph &g);
 
